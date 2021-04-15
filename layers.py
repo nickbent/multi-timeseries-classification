54b0f0b335f98e1a7376a2dc6cfb3f0230432725
Nicolas Bent <chocholatethunder@Nicolass-MacBook-Air.local>
2021-04-14 23:18:35 -0400

    layers

---
+++ layers.py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+
+def cnn1dblock(num_channels, out_channels, kernel_size, stride = 2,  activation = True, padding = 2):
+
+    layers = []
+    layers += nn.Conv1d(num_channels, out_channels, kernel_size, stride=stride, padding = 2)
+    layers += nn.BatchNorm1d(out_channels)
+    if activation:
+        layers += nn.ReLU()
+    
+    return nn.Sequential(*layers)
+
+
+def linear_layer(in_features = 300, out_features = 300 , batch_norm = False, drop_out = 0.5):
+    layer = []
+    if batch_norm:
+        layer.append(nn.BatchNorm1d(num_features=in_features))
+    if drop_out != 0 :
+        layer.append(nn.Dropout(p=drop_out))
+    layer.append( nn.Linear(in_features= in_features, out_features=out_features))
+    return layer
+
+
+
